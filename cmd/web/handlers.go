95493ceb5ab2e31a0cf95dad003503499cf67a20
Giacomo <gbonomelli483@gmail.com>
2026-04-09 14:05:43 +0200

    Added the parsing and execution of the template in the home handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,15 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from Snippetbox"))
+// Use the template.ParseFiles() function to read the template file into a
+// template set. If there's an error, we log the detailed error message, use
+// the http.Error() function to send an Internal Server Error response to the
+// user, and then return from the handler so no subsequent code is executed.
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w,"Internal Server Error", http.StatusInternalServerError)
+	return
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
